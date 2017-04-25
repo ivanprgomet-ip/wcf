c5bac671c519be32b17c2995bd91a45f7e4c42e7
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-04-25 18:28:01 +0200

    consuming a dot net web service from webform application. in this case we are offered interopability because of using the http protocol with the xlm message format. this might for e.g. be a specific client requirement. this way is deprecated.

---
+++ WebServicesDemo/HelloWebClient/UI.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWebClient
+{
+    public partial class UI : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HelloWebServiceNamespace.HelloWebServiceSoapClient client = new HelloWebServiceNamespace.HelloWebServiceSoapClient();
+            Label1.Text = client.Hello(TextBox1.Text);
+        }
+    }
+}
---
+++ WebServicesDemo/WebServicesDemo/HelloWebService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServicesDemo
+{
+    /// <summary>
+    /// Summary description for HelloWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class HelloWebService : System.Web.Services.WebService
+    {
+        // this is the web service!
+        [WebMethod]
+        public string Hello(string who)
+        {
+            return "Hello " + who;
+        }
+    }
+}
