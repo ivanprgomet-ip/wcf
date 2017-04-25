d944de5926e93df4cf104bcb7ee530b39c0e149f
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-04-25 19:32:29 +0200

    consuming a dot net remoting service. this is also deprecated. this time for example for another client requirement with TCP protocol and binary message format implementation because binary over tcp offers better performance.

---
+++ IHelloRemotingService/HelloRemotingServiceClient/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+
+namespace HelloRemotingServiceClient
+{
+    public partial class Form1 : Form
+    {
+        IHelloRemotingService.IHelloRemotingService client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            TcpChannel channel = new TcpChannel();
+            ChannelServices.RegisterChannel(channel);
+            client = (IHelloRemotingService.IHelloRemotingService)Activator.GetObject
+                (typeof(IHelloRemotingService.IHelloRemotingService),"tcp://localhost:8080/Hello");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /// you first go into the C:\Users\Ivan\Source\Repos\wcf\IHelloRemotingService\RemotingServiceHost\bin\Debug
+            /// and run the .exe to make the service available. then you consume it from the HelloRemotingServiceClient winform!
+
+            label1.Text = client.Hello(textBox1.Text);
+        }
+    }
+}
