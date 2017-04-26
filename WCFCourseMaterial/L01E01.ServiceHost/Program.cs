892e3d01408e6a044bf7d4f5d32d5792ec2b3ba8
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-04-26 16:21:10 +0200

    lesson 01 exercise 01 working

---
+++ WCFCourseMaterial/L01E01.ServiceHost/Program.cs
@@ -0,0 +1,29 @@
+﻿using L01E01.ServiceHost.GreetServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01E01.ServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GreetServiceClient service = new GreetServiceClient("BasicHttpBinding_IGreetService");
+            Name current = new Name();
+
+            Console.WriteLine("Enter firstname: ");
+            current.Firstname = Console.ReadLine();
+
+            Console.WriteLine("Enter lastname: ");
+            current.Lastname = Console.ReadLine();
+
+            Console.WriteLine("working");
+            Console.WriteLine(service.Greet(current));
+
+            Console.ReadKey();
+        }
+    }
+}
--- WCFCourseMaterial/L01E01.ServiceHost/Properties/AssemblyInfo.cs
+++ WCFCourseMaterial/L01E01.ServiceHost/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("L01E01.Client")]
+[assembly: AssemblyTitle("L01E01.ServiceHost")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("L01E01.Client")]
+[assembly: AssemblyProduct("L01E01.ServiceHost")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3d897cdf-fec3-4d56-8edf-8aac997517e0")]
+[assembly: Guid("7076e30c-89e7-4ea0-8cf2-4babc4038930")]
 
 // Version information for an assembly consists of the following four values:
 //
