ce6e87b4b846af6516e58287f9fc16c06e920add
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-04-26 11:48:27 +0200

    lesson 01 exercise 01 done. had to run visual studio as admin to make the wcf service client window appear and work.

---
+++ WCFCourseMaterial/L01E01.ServiceLibrary/Service.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01E01.ServiceLibrary
+{
+    public class GreetService : IGreetService
+    {
+        public string Greet(Name obj)
+        {
+            return "Hello " + obj.Firstname + " " + obj.Lastname;
+        }
+    }
+
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string Firstname { get; set; }
+        [DataMember]
+        public string Lastname { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IGreetService
+    {
+        [OperationContract]
+        string Greet(Name obj);
+    }
+}
