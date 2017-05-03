e89192cda42007d70c3c57046f7cc8dfbe037e8a
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-03 11:12:26 +0200

    geo weather service

---
+++ GeoWeatherService/GeoWeatherService.ConsoleClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoWeatherService.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference.GlobalWeatherSoapClient client = new ServiceReference.GlobalWeatherSoapClient("GlobalWeatherSoap");
+
+            string Country = "Croatia";
+
+            string cities = client.GetCitiesByCountry(Country);
+
+            Console.WriteLine(cities);
+
+        }
+    }
+}
