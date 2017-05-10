fa58032dfb09fec8b99a59a1e5d8033327544502
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-10 19:09:49 +0200

    annotations on ievalservice operationcontract methods

--- WCFCourseMaterial/L04E03.RESTfullExercise.EvalServiceLibrary/EvalService.cs
+++ WCFCourseMaterial/L04E03.RESTfullExercise.EvalServiceLibrary/EvalService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 
 namespace L04E03.RESTfullExercise.EvalServiceLibrary
@@ -12,9 +13,24 @@ namespace L04E03.RESTfullExercise.EvalServiceLibrary
     {
         static List<Eval> evals = new List<Eval>();
 
+        public Eval GetEval(string id)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Eval> GetEvals()
         {
             return evals;
+        }
+
+        public List<Eval> GetEvalsBySybmitter(string submitter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveEval()
+        {
+            throw new NotImplementedException();
         }
 
         public void SubmitEval(Eval eval)
@@ -28,10 +44,22 @@ namespace L04E03.RESTfullExercise.EvalServiceLibrary
     [ServiceContract]
     public interface IEvalService
     {
+        [WebInvoke(Method="POST",UriTemplate="evals")] // servicemodel.web
         [OperationContract(IsOneWay = true)]
         void SubmitEval(Eval eval);
         [OperationContract]
+        [WebGet(UriTemplate ="evals")]
         List<Eval> GetEvals();
+
+        [OperationContract]
+        [WebGet(UriTemplate ="eval/{id}")]
+        Eval GetEval(string id);
+        [OperationContract]
+        [WebGet(UriTemplate ="evals/{submitter}")]
+        List<Eval> GetEvalsBySybmitter(string submitter);
+        [OperationContract]
+        [WebInvoke(Method ="DELETE",UriTemplate ="eval/{id}")]
+        void RemoveEval();
     }
 
     [DataContract(Namespace = "http://localhost:8080/evals")]
