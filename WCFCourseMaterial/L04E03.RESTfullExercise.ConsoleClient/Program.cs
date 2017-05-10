39361289f60692f17f71e7bb851a37bba47337eb
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-10 22:00:07 +0200

    client

---
+++ WCFCourseMaterial/L04E03.RESTfullExercise.ConsoleClient/Program.cs
@@ -0,0 +1,71 @@
+﻿using L04E03.RESTfullExercise.EvalServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L04E03.RESTfullExercise.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebChannelFactory<IEvalService> cf = new WebChannelFactory<IEvalService>(
+                 new Uri("http://localhost:8080/evalservice"));
+
+            IEvalService client = cf.CreateChannel();
+
+
+            while (true)
+            {
+                Console.WriteLine("Enter a commando >> ");
+                string comm = Console.ReadLine();
+
+                switch (comm)
+                {
+                    case "submit":
+                        Eval e = new Eval()
+                        {
+                            Comments = "hahahahah",
+                            Submitter = "ivannooo",
+                            TimeSent = DateTime.Now,
+                            Id = "394853458",
+                        };
+
+                        client.SubmitEval(e);
+                        Console.WriteLine("Created!");
+                        break;
+                    case "get":
+                        Console.WriteLine("enter a id");
+                        string input = Console.ReadLine();
+
+                        Eval retrieved = client.GetEval(input);
+                        Console.WriteLine("Entered: ");
+                        Console.WriteLine(retrieved.Comments + ' ' + retrieved.Submitter);
+                        break;
+                    case "list":
+                        Console.WriteLine("Enter a subbmitter >>");
+                        string submitterInput = Console.ReadLine();
+
+                        client.GetEvalsBySybmitter(submitterInput);
+
+                        break;
+                    case "remove":
+                        Console.WriteLine("Id to remove >> ");
+                        string removeId = Console.ReadLine();
+                        client.RemoveEval(removeId);
+                        break;
+                    default:
+                        Console.WriteLine("Command is not supported!");
+                        break;
+                }
+                if (comm == "exit")
+                    break;
+            }
+
+            Console.WriteLine("exiting program");
+        }
+    }
+}
