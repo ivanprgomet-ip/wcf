19ed38fac3534e660f6a363f3841211cb8324582
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-12 13:49:04 +0200

    repetition exercise 1

---
+++ Re.E01L01/ServiceLibrary/PersonService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLibrary
+{
+    public class PersonService : IPersonService
+    {
+        public string GetFullname(Name name)
+        {
+            return name.Firstname + ' ' + name.Lastname;
+        }
+    }
+
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string Firstname { get; set; }
+        [DataMember]
+        public string Lastname { get; set; }
+
+    }
+
+    [ServiceContract]
+    interface IPersonService
+    {
+        [OperationContract]
+        string GetFullname(Name name);
+    }
+}
