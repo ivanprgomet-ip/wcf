ab93aee17e5022fca78af474af4c3a278cd6415b
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-15 13:54:29 +0200

    created eval service project

---
+++ Re.SelfHosting/ServiceLibrary/EvalService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLibrary
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    class EvalService : IEvalService
+    {
+        List<Eval> evals = new List<Eval>();
+        public List<Eval> GetEvals()
+        {
+            return evals;
+        }
+
+        public void SubmitEval(Eval eval)
+        {
+            if (eval != null)
+                evals.Add(eval);
+            else
+                Console.WriteLine("the eval is null");
+        }
+    }
+
+    [DataContract]
+    public class Eval
+    {
+        [DataMember]
+        public string Submitter { get; set; }
+        [DataMember]
+        public DateTime TimeSent { get; set; }
+        [DataMember]
+        public string Comments { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IEvalService
+    {
+        [OperationContract]
+        void SubmitEval(Eval eval);
+        List<Eval> GetEvals();
+
+    }
+}
