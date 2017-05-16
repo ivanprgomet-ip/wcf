8492a218b5277b068082ec4e8c49eeee52c21423
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-16 21:54:45 +0200

    some work

---
+++ RepetitionExercises/E02.MediaHost/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E02.MediaHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// [x] enable HTTP-get (see appconfig in MyMediaServiceLibrary)
+            /// [] set HTTP-get-url to http://localhost:8080/mediaService/meta
+            /// [] create bindings: 
+            ///     basicHttpBinding
+            ///     WsHttpBinding
+            ///     netTcpBinding
+
+            ServiceReference.MediaServiceClient client = new ServiceReference.MediaServiceClient();
+
+        }
+    }
+}
