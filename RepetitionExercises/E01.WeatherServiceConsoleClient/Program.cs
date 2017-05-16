a4f49c437e224216eaccd861b0f1f006ac350add
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-16 19:09:20 +0200

    weather service exercise

---
+++ RepetitionExercises/E01.WeatherServiceConsoleClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E01.WeatherServiceConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference.GlobalWeatherSoapClient client = new ServiceReference.GlobalWeatherSoapClient("GlobalWeatherSoap");
+            string Country = "Germany";
+            string citiesReturned = client.GetCitiesByCountry(Country);
+
+            Console.WriteLine(Country+": ");
+            Console.WriteLine(citiesReturned);
+        }
+    }
+}
